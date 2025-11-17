724910223f455b711571e877486e140000a5518d
ahmadlatifdev <privategate777@gmail.com>
2025-11-17 13:01:01 -0500

    Simplify next.config.js with SWC minification
    Removed image domains and experimental settings.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['cdn.cjdropshipping.com', 'img.alicdn.com', 'yourstore.com'],
-  },
-  experimental: {
-    serverActions: true,
-  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
