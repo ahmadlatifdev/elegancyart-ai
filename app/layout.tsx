72b9e49d20e6afacbdf9b444d05c726c5752b1a9
ahmadlatifdev <privategate777@gmail.com>
2025-11-20 15:08:37 -0500

    Remove RootLayout component from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,1 @@
-import React from "react";
 
-export const dynamic = "force-dynamic";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
