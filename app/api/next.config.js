d296ee8e8722cc348f4fb8c2ac0d1aa323832cef
ahmadlatifdev <privategate777@gmail.com>
2025-11-20 15:06:36 -0500

    Remove Next.js configuration file

--- app/api/next.config.js
+++ app/api/next.config.js
@@ -1,12 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['cdn.cjdropshipping.com', 'img.alicdn.com', 'yourstore.com'],
-  },
-  experimental: {
-    serverActions: true,
-  },
-};
 
-module.exports = nextConfig;
