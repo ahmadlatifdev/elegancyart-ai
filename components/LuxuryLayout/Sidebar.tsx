093547d7ce56f68ba7e9c9c2e3545b2d61a23b24
ahmadlatifdev <privategate777@gmail.com>
2025-12-17 20:22:10 -0500

    Refactor Sidebar component to use <aside> element

--- components/LuxuryLayout/Sidebar.tsx
+++ components/LuxuryLayout/Sidebar.tsx
@@ -1,3 +1,7 @@
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <aside style={{ padding: 16, borderRight: "1px solid #222", minHeight: "100vh" }}>
+      Sidebar
+    </aside>
+  );
 }
