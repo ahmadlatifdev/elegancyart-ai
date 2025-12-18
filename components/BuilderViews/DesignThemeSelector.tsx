a80ff066adcc56043a1677d87112b7cdd86e415e
ahmadlatifdev <privategate777@gmail.com>
2025-12-17 20:26:49 -0500

    Add DesignThemeSelector component

--- components/BuilderViews/DesignThemeSelector.tsx
+++ components/BuilderViews/DesignThemeSelector.tsx
@@ -1,1 +1,8 @@
+export default function DesignThemeSelector() {
+  return (
+    <section style={{ padding: 16, border: "1px solid #222", borderRadius: 8 }}>
+      DesignThemeSelector
+    </section>
+  );
+}
 
