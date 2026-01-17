add4d8e2b7426bf405e105baf84c7910813723e9
ahmadlatifdev <privategate777@gmail.com>
2026-01-16 22:29:19 -0500

    Refactor BuilderPanel to use implicit return

--- components/BuilderPanel.jsx
+++ components/BuilderPanel.jsx
@@ -7,7 +7,7 @@ import DesignThemeSelector from './BuilderViews/DesignThemeSelector';
 import ProductPreviewPanel from './BuilderViews/ProductPreviewPanel';
 import ProductSearchWizard from './ProductSearchWizard';
 
-const BuilderPanel: React.FC = () => {
+const BuilderPanel = () => {
   const [activePanel, setActivePanel] = useState('dashboard');
 
   const renderPanel = () => {
@@ -29,7 +29,7 @@ const BuilderPanel: React.FC = () => {
 
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="flex gap-4 p-4 bg-gray-900 text-white justify-start">
+      <div className="flex gap-4 p-4 bg-gray-900 text-white justify-start flex-wrap">
         <button onClick={() => setActivePanel('dashboard')}>Dashboard</button>
         <button onClick={() => setActivePanel('recommendations')}>AI Recommendations</button>
         <button onClick={() => setActivePanel('themes')}>Theme Selector</button>
