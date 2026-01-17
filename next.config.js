8676a9fef098f914c12933634aff5911a5c1cca8
ahmadlatifdev <privategate777@gmail.com>
2026-01-16 20:14:55 -0500

    next.config.js
    Removed output and trailingSlash options from nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
+  typescript: {
+    ignoreBuildErrors: true
   },
   eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+    ignoreDuringBuilds: true
+  }
 };
 
 module.exports = nextConfig;
