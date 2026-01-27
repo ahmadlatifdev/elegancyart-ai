52996e9de6edca39064a1a5046efef6093f7465f
ahmadlatifdev <privategate777@gmail.com>
2026-01-27 18:41:17 -0500

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: false,
+  reactStrictMode: true,
+
+  // Bunny CDN Pull Zone
+  assetPrefix: "https://bossmindcdn.b-cdn.net",
+
   images: {
-    unoptimized: true
-  }
+    loader: "default",
+    domains: ["bossmindcdn.b-cdn.net"],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
